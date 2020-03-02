c2737046b4ae1d05e0bf90d02d7453e5ed926767
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-02 21:08:59 +0000

    working

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,18 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our", "their", "his", "her"];
+  var adj = ["great", "big", "gigantic", "scary", "furry", "friendly"];
+  var noun = ["cat", "dog", "helper", "friend", "developer"];
+
+  let emptyArray = [];
+  for (let i in pronoun)
+    for (let b in adj)
+      for (let c in noun)
+        emptyArray.push(
+          "http://www." + pronoun[i] + adj[b] + noun[c] + ".com <br>"
+        );
+  console.log(emptyArray);
+  document.querySelector(".domains").innerHTML = emptyArray;
 };
