66a7b8746bfe5ab58d0570670ec049906eac8098
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-07-11 13:59:20 +0000

    added extra string

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
@@ -9,14 +8,16 @@ window.onload = function() {
   let pronoun = ["the", "our", "their", "his", "her"];
   let adj = ["great", "friendly", "gigantic", "scary", "invisible"];
   let noun = ["engineer", "man", "helper", "friend", "developer"];
+  let ext = [".com", ".net", ".edu", ".io", ".org"];
 
   let emptyArray = [];
-  for (let i in pronoun)
+  for (let a in pronoun)
     for (let b in adj)
       for (let c in noun)
-        emptyArray.push(
-          "http://www." + pronoun[i] + adj[b] + noun[c] + ".com <br>"
-        );
+        for (let d in ext)
+          emptyArray.push(
+            "http://www." + pronoun[a] + adj[b] + noun[c] + ext[d] + "<br>"
+          );
 
   console.log(emptyArray);
 
