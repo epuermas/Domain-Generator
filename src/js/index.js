c112171e663f924d8cd38ac9e8673b47e1e4ee33
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-07-11 12:35:00 +0000

    rework for class

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@
 import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
