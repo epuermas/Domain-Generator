93eb4e0b8547ac0e711523eaa323e0c290903294
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-07-15 13:32:46 +0000

    comments added

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,16 @@ import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
+/* Declaring the function */
 window.onload = function() {
   let pronoun = ["the", "our", "their", "his", "this", "her"];
   let adj = ["great", "friendly", "gigantic", "scary", "invisible"];
   let noun = ["engineer", "man", "helper", "friend", "developer"];
   let ext = [".com", ".net", ".edu", ".io", ".org"];
 
+  /* Create new empty array and loop all possible names */
   let emptyArray = [];
+  /* Concatinate all possible names */
   for (let a in pronoun)
     for (let b in adj)
       for (let c in noun)
@@ -17,6 +20,7 @@ window.onload = function() {
           emptyArray +=
             "http://www." + pronoun[a] + adj[b] + noun[c] + ext[d] + "<br>";
 
+  /* Return the domains */
   console.log(emptyArray);
 
   document.querySelector(".domains").innerHTML = emptyArray;
